057ccd5d7631e65350156e9ba00310208093443a
Romir <romir.tandon@gmail.com>
2022-10-17 10:31:29 -0400

    Finished up the Game, I think its kinda cool, but ofcouse it could be better Archived -Romir

--- src/events.java
+++ src/events.java
@@ -15,34 +15,66 @@ public class events {
         phealth = gphealth;
     }
 
-    public void startEvent(){
+    public void startEvent() throws InterruptedException {
         System.out.println("=======================");
         System.out.println("Something Important Happened!: "+prompt);
+        int startHealth = phealth;
         for(int i=0; i<enemyAmount; i++){
-            String ename = enemyNames[random.nextInt(5)];
-            int ehealth = random.nextInt(10);
-
-            System.out.println("You are facing "+ ename+"...He has "+ehealth+" health.");
-
+            String ename = enemyNames[random.nextInt(enemyNames.length)];
+            int ehealth = random.nextInt(10)+1;
+            phealth = startHealth;
+            System.out.println("You are facing "+ ename+"...He has "+ehealth+" health, you have "+phealth+" health.");
+            Thread.sleep(100);
             while(ehealth>0){
                 Scanner input = new Scanner(System.in);
-                System.out.println("Do you want to fight(1) or heal(2)?: ");
+                System.out.println("Do you want to fight(1) or heal(2) or run(3) or do nothing(4)?: ");
+                Thread.sleep(100);
                 try{
+                    if (phealth ==0){
+                        System.out.println("You ran out of health you noob, lets try again with another dude... \n\n");
+                        Thread.sleep(100);
+                        ehealth=0;
+                        break;
+                    }
                     switch(input.nextInt()){
                         case 1:
-                            System.out.println("You were attacked you lost 1 health, your enemy is at "+ehealth+"health!");
+                            System.out.println("You were attacked you lost 1 health, your enemy is at "+ehealth+" health!");
+                            Thread.sleep(100);
                             phealth -=1;
                             System.out.println("You attacked with your sharpened pinky! You deal 2 damage!");
+                            Thread.sleep(100);
                             ehealth -=2;
                             System.out.println("Enemy is now at "+ ehealth+" health!");
+                            Thread.sleep(100);
                             break;
                         case 2:
-                            System.out.println("You were attacked you lost 1 health, your enemy is at "+ehealth+"health!");
+                            System.out.println("You were attacked you lost 1 health, your enemy is at "+ehealth+" health!");
+                            Thread.sleep(100);
                             phealth -=1;
-                            System.out.println("You wrapped gauze on your forearms, I don't see how that helps. You heal 3 health pts!");
-
+                            System.out.println("You wrapped gauze on your forearms, I don't see how that helps. You heal 3 health pts! You're at "+phealth+" health!");
+                            Thread.sleep(100);
+                            break;
+                        case 3:
+                            if (ehealth<5){
+                                System.out.println("He was too injured to notice you dive into the bush and hide, he walks away after a couple of minutes off into the sunset!");
+                                Thread.sleep(100);
+                                ehealth = 0;
+                            }else{
+                                System.out.println("You dive into a bush but he can still you, he kicks the bush and you take 1 damage!");
+                                Thread.sleep(100);
+                                phealth -=1;
+                            }
+                        case 4:
+                            System.out.println("You kinda just stand there?");
+                            Thread.sleep(100);
+                            System.out.println("You were attacked you lost 1 health, your enemy is at "+ehealth+" health!");
+                            Thread.sleep(100);
+                            phealth -=1;
+                            break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + input.nextInt());
                     }
-                }catch(InputMismatchException errorOut){
+                }catch(InputMismatchException | InterruptedException errorOut){
                     System.out.println("You got this wacky error (hint: you didn't give me a number in the parentheses please get better at this!): ");
                 }
             }
--- src/playerCharacter.java
+++ src/playerCharacter.java
@@ -9,6 +9,7 @@ public class playerCharacter {
 
     public events event1 = new events(1,3,10,"You're car is broken down on the side of the road, three creatures appear from the dark!");
     public events event2 = new events(2,1,3,"Another creature blocks your way back to the car!");
+    public events event3 = new events(3,8,20,"While fixing your carburator, something taps on your shoulder... A horde appears");
 
     public playerCharacter(String gname, int glevel){
         name = gname;
@@ -24,18 +25,31 @@ public class playerCharacter {
         level +=1;
     }
 
-    public void eventsPlay(){
+    public void eventsPlay() throws InterruptedException {
         System.out.println("==============");
+        Thread.sleep(100);
         System.out.println("You've got to go down this road buddy....the journey begins!");
+        Thread.sleep(100);
         event1.startEvent();
+        Thread.sleep(100);
         event2.startEvent();
+        System.out.println("==============");
+        Thread.sleep(100);
+        System.out.println("You gotta go down this big highway, and you make it to the end, taking a left your camp is in sight, but the shoddy craftmanship that you did to fix your car is coming undone and you have to stop to repair again!");
+        event3.startEvent();
+        Thread.sleep(100);
+        System.out.println("\n\n==============");
+        Thread.sleep(100);
+        System.out.println("You reach your camp, congratulations you're just that good!");
+        Thread.sleep(100);
+        System.out.println("==============");
     }
 
 
     public void startingScreen() {
         Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter 1 to play and 0 to exit, anything else and you're bad at this!");
+            System.out.print("Enter 1 to play and 0 to exit, anything else and you're bad at this:  ");
             try {
                 switch (input.nextInt()) {
                     case 1:
@@ -48,7 +62,7 @@ public class playerCharacter {
                     default:
                         System.out.println("Honestly just do better really man.....");
                 }
-            } catch (InputMismatchException errorOut) {
+            } catch (InputMismatchException | InterruptedException errorOut) {
                 System.out.println("You got this wacky error (hint: you didn't give me a number): " + errorOut);
             }
         }
